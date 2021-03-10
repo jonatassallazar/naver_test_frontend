61d3a9d2fd4a2070d9c3a3dd94c847264121e2f4
Jonatas Sallazar <jonatassallazar@gmail.com>
2021-03-09 22:07:53 -0300

    set Bearer token default

--- src/axios/instance.js
+++ src/axios/instance.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('@navers_test/bearer_id')
+
+axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+
 export default axios.create({
     baseURL: 'https://navedex-api.herokuapp.com/v1',
   })
